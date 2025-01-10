0a294f5cd8af01744807705c1d8d0ee4612b511a
Nizar Sauas <samisauas97@gmail.com>
2025-01-10 21:04:36 +0200

    new update

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("open world");
+// Age Category Write a program that takes a person's age as input and determines their age category:
+
+/*function determine(age) {
+  let result = "";
+  if (age < 13) {
+    result = "Child";
+  } else if (13 < age < 20) {
+    result = "Teenager";
+  } else if (20 < age < 60) {
+    result = "Adult";
+  } else if (age > 60)
+  result = 'senior';
+  
+  return result;
+}
+console.log(determine(14)); */
+
+// Speed Fine Calculator Write a program that calculates the fine for speeding based on the speed limit and the vehicle's speed:
+/*  DID NOT WORK 
+function determine() {
+  let result = '';
+  let speedLimit = 80;
+  let vehicleSpeed;
+  if (vehicleSpeed >= speedLimit) {
+    text = "No fine";
+    // If the speed is over the limit by 1–20 km/h, print "Fine: $50."
+  } else if (vehicleSpeed > speedLimit) {
+    text = "Fine: $50";
+    // If the speed is over the limit by 21–40 km/h, print "Fine: $150."
+  } else if (vehicleSpeed > speedLimit) {
+    text = "Fine: $150";
+    // If the speed exceeds the limit by more than 40 km/h, print "Fine: $500."
+  } else if (vehicleSpeed > speedLimit) {
+    text = "Fine: $500";
+    return text;
+  }
+}
+console.log(determine, text());
+*/
+
+// Day of the Week Write a program that takes a number (1 to 7) as input and prints the corresponding day of the week:
+
+/* function calendar(day) {
+  let result;
+  switch (day) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3: 
+      return 'Wensday'
+    case 4: 
+      return 'Thursday'
+    case 5: 
+      return 'Friday'
+  }
+
+  return day;
+}
+console.log(calendar(2)); */
